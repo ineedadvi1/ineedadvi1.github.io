b62f770e5280908e796d3a3aeca48c96e881170d
advik sinha <94247337+ineedadvi1@users.noreply.github.com>
2024-03-25 19:00:20 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
   // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-  VITE_CORS_PROXY_URL: "",
+  VITE_CORS_PROXY_URL: "https://simple-proxy.procadvik9.workers.dev",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OTgzNjVlYTBlOWY3MzMyYmE4ZDdhYTc3NWRhOGVmMCIsInN1YiI6IjY2MDFmODdiMDkyOWY2MDE3ZTllMGYxZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hWFh9CNNuHjdJZWliWR-DmVCUO5L9uqOlsf6U_EvriY",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
